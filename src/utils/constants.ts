c383cab6f921c809776f510798aa0ef732d3dd8d
Nikhil <nikhil@tartanhq.com>
2023-07-04 15:21:39 +0530

    first commit

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,12 +1,3 @@
 export const BASE_URL = "https://api.staging.equal.in";
 
-export const header = {
-  accept: "*/*",
-  "accept-language": "en-GB,en;q=0.6",
-  // "access-control-allow-origin": "*",
-  // "cache-control": "no-cache",
-  "content-type": "application/json; charset=utf-8",
-  pragma: "no-cache",
-  authority: "api.test.equal.in",
-  "x-eq-requester-id": "ID_GATEWAY",
-};
+export const header = {};
