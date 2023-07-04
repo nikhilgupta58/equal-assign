497704518d061ba38171de5aaa1853a5e6727ade
Nikhil <nikhil@tartanhq.com>
2023-07-04 14:20:44 +0530

    first commit

--- src/hooks/useVerifyToken.tsx
+++ src/hooks/useVerifyToken.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { getProtectedAxios } from "../libs/auth";
-import { userStore } from "../store";
+import { toastStore, userStore } from "../store";
 import { BASE_URL } from "../utils/constants";
 
-export default function useRequestToken() {
+export default function useVerifyToken() {
   const [isSuccess, setIsSuccess] = React.useState(null);
   const [isError, setIsError] = React.useState(null);
+  const [setErrorToast] = toastStore((state) => [state.setErrorToast]);
   const [isLoading, setIsLoading] = React.useState(false);
   const setSession = userStore((state: any) => state.setSession);
   const mutate = ({ mobileNumber, mobileOtp }) => {
@@ -21,9 +22,12 @@ export default function useRequestToken() {
     const endpoint = BASE_URL + "/auth/verify";
     axios
       .post(endpoint, payload)
-      .then(({ data }) => {
+      .then(({ data }: { data: any }) => {
         setIsSuccess(true);
-        console.log(data);
+        if (data?.status_code === "200") setIsSuccess(true);
+        else {
+          setErrorToast(data?.message);
+        }
         // const session = `EQSI=EG-PR-MMNTTMPXVWXHC1PKMW51;EQTS=$2a$10$gXusaQ3eLsNyPdM7ZLKVOeFV35BHmUDbfAurUL6R4HzuRhB5TY4Mi;ESTS=gPNdVR7Gn9cvdrvfmrXE5DOSOcgA8DWWg9rxlqMfSOH8NPOGbQSKxUCvvkfwGjlW0cp8x5x-CZNl8EonJFdYBg`;
         // setSession(session);
         setSession(null);
--- src/pages/Login/components/LoginModal.tsx
+++ src/pages/Login/components/LoginModal.tsx
@@ -3,15 +3,14 @@ import { RxCross2 } from "react-icons/rx";
 import Button from "../../../components/Button";
 import Modal from "../../../components/Modal";
 import useRequestToken from "../../../hooks/useRequestToken";
+import useVerifyToken from "../../../hooks/useVerifyToken";
 import { validatePhone } from "../../../utils/helper";
 
 export default function LoginModal({ isOpen, onClose }) {
   const { mutate, isLoading, isSuccess } = useRequestToken();
+  const { mutate: verifyMutate, isLoading: isVerifyLoading } = useVerifyToken();
   const [mobileNumber, setMobileNumber] = React.useState("");
-
-  React.useEffect(() => {
-    if (isSuccess) console.log(isSuccess);
-  }, [isSuccess]);
+  const [mobileOtp, setMobileOtp] = React.useState("");
 
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
@@ -21,26 +20,51 @@ export default function LoginModal({ isOpen, onClose }) {
           <p>Mobile Number</p>
           <RxCross2 onClick={onClose} />
         </div>
-        <div className="p-4">
-          <input
-            value={mobileNumber}
-            onChange={(e) => {
-              setMobileNumber(e.target.value);
-            }}
-            placeholder="Enter phone number"
-            type="number"
-            className="p-2 bg-gray-50 w-full border-[1px] border-gray-100 appearance-none"
-          />
-          <p className="text-[12px] text-gray-400">Enter your no. to login</p>{" "}
-          <Button
-            isLoading={isLoading}
-            onClick={() => mutate(mobileNumber)}
-            className="w-full mt-4"
-            disabled={!validatePhone(mobileNumber)}
-          >
-            Proceed
-          </Button>
-        </div>
+        {!isSuccess ? (
+          <div className="p-4">
+            <input
+              value={mobileOtp}
+              onChange={(e) => {
+                setMobileOtp(e.target.value);
+              }}
+              placeholder="Enter otp"
+              type="number"
+              className="p-2 bg-gray-50 w-full border-[1px] border-gray-100 appearance-none"
+            />
+            <p className="text-[12px] text-gray-400">
+              Enter otp sent to {mobileNumber}
+            </p>
+            <Button
+              isLoading={isVerifyLoading}
+              onClick={() => verifyMutate({ mobileNumber, mobileOtp })}
+              className="w-full mt-4"
+              disabled={mobileOtp?.length !== 6}
+            >
+              Proceed
+            </Button>
+          </div>
+        ) : (
+          <div className="p-4">
+            <input
+              value={mobileNumber}
+              onChange={(e) => {
+                setMobileNumber(e.target.value);
+              }}
+              placeholder="Enter phone number"
+              type="number"
+              className="p-2 bg-gray-50 w-full border-[1px] border-gray-100 appearance-none"
+            />
+            <p className="text-[12px] text-gray-400">Enter your no. to login</p>{" "}
+            <Button
+              isLoading={isLoading}
+              onClick={() => mutate(mobileNumber)}
+              className="w-full mt-4"
+              disabled={!validatePhone(mobileNumber)}
+            >
+              Proceed
+            </Button>
+          </div>
+        )}
       </div>
     </Modal>
   );
