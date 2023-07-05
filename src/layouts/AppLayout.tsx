1d8f67b49f7350a0f8b64acf8cdd19b1389639df
Nikhil <nikhil@tartanhq.com>
2023-07-05 13:03:51 +0530

    layout

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -8,7 +8,7 @@ export default function AppLayout({ children }) {
     <GenricLayout>
       <div className="flex flex-col justify-between h-full">
         <div>
-          <div className="my-4 border-b-[1px] border-b-gray-200 flex items-center justify-between px-4 font-[500] text-[20px]">
+          <div className="fixed w-[450px] max-w-[100vw] bg-white pt-4 pb-2 border-b-[1px] border-b-gray-200 flex items-center justify-between px-4 font-[500] text-[20px]">
             <div className="w-1 h-11" />
             <div className="flex flex-col gap-3 pb-[20px]">
               <img src={logo} className="object-contain max-h-[37px]" />
@@ -20,6 +20,7 @@ export default function AppLayout({ children }) {
               className="cursor-pointer"
             />
           </div>
+          <div className="w-full h-[120px]" />
           <div className="p-4 flex flex-col gap-4">{children}</div>
         </div>
       </div>
